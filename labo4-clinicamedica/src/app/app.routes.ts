861a28d99b8a0182bbe7911e5027273e3b8437af
Fran Diaz B <frandiazb97@gmail.com>
2025-10-15 10:26:10 -0300

    commit

---
+++ labo4-clinicamedica/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { flush } from '@angular/core/testing';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+    {path: '', redirectTo: '/welcome', pathMatch: "full"}, 
+    {path: 'welcome', loadComponent: ()=> import('./componentes/welcome/welcome.component').then(m => m.WelcomeComponent)}, 
+    {path: 'login', loadComponent: ()=> import('./componentes/login/login.component').then(m => m.LoginComponent)},
+    {path: 'register', loadComponent: ()=> import('./componentes/register/register.component').then(m => m.RegisterComponent)},
+
+];
